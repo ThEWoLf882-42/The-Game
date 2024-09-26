89560d50f43e05d902a6c92086f4d72f4023dab4
ThEWoLf882-42 <agimi@student.1337.ma>
2024-09-26 13:21:25 +0100

    added ID

--- Home.js
+++ Home.js
@@ -1,24 +1,24 @@
 export const HOME = `<div class="property-11">
-	<div class="square">
+	<div class="square" id="HOME">
 		<img class="vs-1-icon" alt="" src="/textures/svg/VS 1.svg" />
 		<div class="square1">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home1">Home</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="GAME">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home2">Game</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="CHAT">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home3">Chat</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="LEADERBOARD">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home4">Leaderboard</div>
@@ -27,26 +27,26 @@ export const HOME = `<div class="property-11">
 </div>`;
 
 export const GAME = `<div class="property-11">
-	<div class="square2">
+	<div class="square2" id="HOME">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home2">Home</div>
 		</div>
 	</div>
-	<div class="square">
+	<div class="square" id="GAME">
 		<img class="vs-1-icon" alt="" src="/textures/svg/VS 1.svg" />
 		<div class="square1">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home1">Game</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="CHAT">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home3">Chat</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="LEADERBOARD">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home4">Leaderboard</div>
@@ -55,26 +55,26 @@ export const GAME = `<div class="property-11">
 </div>`;
 
 export const CHAT = `<div class="property-11">
-	<div class="square2">
+	<div class="square2" id="HOME">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home2">Home</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="GAME">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home2">Game</div>
 		</div>
 	</div>
-	<div class="square">
+	<div class="square" id="CHAT">
 		<img class="vs-1-icon" alt="" src="/textures/svg/VS 1.svg" />
 		<div class="square1">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home11">Chat</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="LEADERBOARD">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home4">Leaderboard</div>
@@ -83,25 +83,25 @@ export const CHAT = `<div class="property-11">
 </div>`;
 
 export const LEADERBOARD = `<div class="property-11">
-	<div class="square2">
+	<div class="square2" id="HOME">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home2">Home</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="GAME">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home2">Game</div>
 		</div>
 	</div>
-	<div class="square2">
+	<div class="square2" id="CHAT">
 		<div class="square3">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
 			<div class="home3">Chat</div>
 		</div>
 	</div>
-	<div class="square">
+	<div class="square" id="LEADERBOARD">
 		<img class="vs-1-icon" alt="" src="/textures/svg/VS 1.svg" />
 		<div class="square31">
 			<img class="vs-1-icon" alt="" src="/textures/svg/Rectangle.svg" />
